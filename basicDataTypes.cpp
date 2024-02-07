5152a3d06872960827e49d217a1c55fac034c8e8
Akhil B <48994987+akhilb1644@users.noreply.github.com>
2024-02-07 16:21:06 -0500

    Add files via upload

---
+++ basicDataTypes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "An integer: ";
+    int integer = 1971;
+    std::cout << integer;
+    std::cout << "\nA double: ";
+    double doub = 15.67;
+    std::cout << doub;
+    std::cout << "\nA boolean: ";
+    bool boolean = true;
+    std::cout << boolean;
+    return 0;
+}
+/*
+Output:
+
+An integer: 1971
+A double: 15.67
+A boolean: 1
+*/
